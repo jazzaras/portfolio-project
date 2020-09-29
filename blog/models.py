f4c527d38420b019e28e1fe333533ae2912e8f23
jazzaras <jazzaras@yahoo.com>
2020-09-30 01:18:08 +0300

    home done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+	tital = models.CharField(max_length=255)
+	pub_date = models.DateTimeField()
+	image = models.ImageField(upload_to='images/')
+	body = models.TextField()
+	
