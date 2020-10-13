a5f06cad211f5931c33c925994b1082aca6202d5
jazzaras <jazzaras@yahoo.com>
2020-10-13 20:26:20 +0300

    'requirements'

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'hebaHEBA12',
+        'PASSWORD': 'django123',
         'HOST':'localhost',
         'PORT':'5432',
 
